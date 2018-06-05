5c3cf47f20c5e1bde807fd10cec1df8bdd1c133b
Alexey Zolotykh <alexey.zolotykh@team.wrike.com>
2018-06-06 01:37:45 +0300

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:refactoring/refactoring.dart' as refactoring;
+
+main(List<String> arguments) {
+  print('Hello world: ${refactoring.calculate()}!');
+}
---
+++ test/refactoring_test.dart
@@ -0,0 +1,8 @@
+import 'package:refactoring/refactoring.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
